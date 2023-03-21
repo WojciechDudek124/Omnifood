2a02b7335098735e05bed564c23925ab9e97cb2e
Wojtek Dudek <wojciech.dudek124@gmail.com>
2023-03-21 09:26:15 +0100

    add sticky navigation bar to project

--- script.js
+++ script.js
@@ -2,13 +2,35 @@ const yearEl = document.querySelector(".year");
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 const allLinks = document.querySelectorAll("a:link");
+const sectionHeroEl = document.querySelector(".section-hero");
+const bodyEl = document.querySelector("body");
 
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (!ent.isIntersecting) {
+      bodyEl.classList.add("sticky");
+    } else {
+      bodyEl.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+
+obs.observe(sectionHeroEl);
+
 const open = () => {
   headerEl.classList.toggle("nav-open");
 };
+
 
 btnNavEl.addEventListener("click", open);
 allLinks.forEach((link) => {
